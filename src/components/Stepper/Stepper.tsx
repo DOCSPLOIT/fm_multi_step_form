21f010841316d0828381afcf768cecadd2adcbeb
docsploit <robertdevasia@outlook.com>
2023-03-01 09:47:27 +0530

    flex style relocation

--- src/components/Stepper/Stepper.tsx
+++ src/components/Stepper/Stepper.tsx
@@ -43,10 +43,10 @@ export default function Stepper(props: any) {
 
     return <StepperContext.Provider value={{ current, next, previous, total ,goto }}>
         <div className="h-full w-full md:flex md:space-x-12">
-            <div className=" h-[172px] md:min-w-[274px]  md:min-h-[568px] flex justify-center md:flex-col overflow-hidden md:pt-10 md:rounded-lg bg-no-repeat bg-[url('./assets/images/bg-sidebar-mobile.svg')] md:bg-[url('./assets/images/bg-sidebar-desktop.svg')] bg-contain">
+            <div className=" h-[172px] md:min-w-[274px]  md:min-h-[568px] flex justify-center md:justify-start md:flex-col overflow-hidden md:pt-10 md:rounded-lg bg-no-repeat bg-[url('./assets/images/bg-sidebar-mobile.svg')] md:bg-[url('./assets/images/bg-sidebar-desktop.svg')] bg-contain">
                 {
                     props.heads.map((text: string, index: number) => {
-                        return <div key={index} className="flex cursor-pointer md:space-x-4 md:px-7 px-3  pt-5 md:pt-auto  " onClick={() => onClickStep(index)}>
+                        return <div key={index} className="flex cursor-pointer md:space-x-4 md:px-7 px-3  pt-5 md:pt-0  " onClick={() => onClickStep(index)}>
                             <div className="mb-8">
                                 <p className={"rounded-full h-[30px] w-[30px] text-center  " + classNameOfNumber(index)}>{index + 1}</p>
                             </div>
